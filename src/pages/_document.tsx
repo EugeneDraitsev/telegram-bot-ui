e8930748159a8d489af35cd2a92e3c4487393a08
Eugene Draitsev <ddrrai@gmail.com>
2019-12-22 23:33:20 +0100

    Added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,26 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i"
             rel="stylesheet"
           />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" href="/favicon.png" />
+          <title>Telegram Bot Stats</title>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1.0, user-scalable=0"
+          />
+          <meta name="theme-color" content="#F5F5F7" data-react-helmet="true" />
+          <meta property="og:image" content="/preview.png" />
+          <meta property="og:url" content="https://telegram-bot-ui.now.sh/" />
+          <meta property="og:image:width" content="512" />
+          <meta property="og:image:height" content="512" />
+          <meta property="og:type" content="website" />
+          <meta property="og:locale" content="en" />
+          <meta property="og:site_name" content="Telegram Bot Stats" />
+          <meta property="og:title" content="Telegram Bot Stats" />
+          <meta property="og:description" content="Chat Statistics for the last 24 hours" />
+          <meta name="docsearch:version" content="2.0" />
+          <meta name="twitter:image" content="/preview.png" />
         </Head>
         <body>
           <Main />
