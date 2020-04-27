4def785071fdbe4e79fdb3e723477b8827825259
Eugene Draitsev <ddrrai@gmail.com>
2020-04-27 20:40:09 +0200

    Deploy attempt 2

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ module.exports = withTM({
   devIndicators: {
     autoPrerender: false,
     env: {
-      AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-      AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+      AWS_ACCESS_KEY_ID: process.env.AwsAccessKey,
+      AWS_SECRET_ACCESS_KEY: process.env.AwsSecretAccessKey,
     },
   },
 })
